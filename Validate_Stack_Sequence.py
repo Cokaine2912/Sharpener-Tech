f1464ea58fb9f9a80d3bc6e7ea8426e8d0ef7030
Niraj Prashant Kamble <97113663+Cokaine2912@users.noreply.github.com>
2023-11-01 17:31:36 +0530

    Update Validate_Stack_Sequence.py

--- Validate_Stack_Sequence.py
+++ Validate_Stack_Sequence.py
@@ -1,20 +1,18 @@
 ## VALIDATE STACK SEQUENCE 
 
-def isValid(self, s) -> bool:
-  d = {")" : "(","]" : "[","}" : "{"}
-  stack = []
-  for ele in s :
-      if not stack and ele in d :
-          return False
+def validateStackSequences(self, pushed, popped) -> bool:
+    stack = []
+    a,b = pushed,popped
+    i = 0
+    j = 0
+    while i < len(a) and j < len(b) :
+        stack.append(a[i])
+        i = i + 1
+        # print(stack)
+        while stack and j < len(b) and stack[-1] == b[j] :
+            # print("Inst")
+            stack.pop()
+            j = j + 1
+            # print(stack)
+    return False if stack else True
 
-      if ele not in d :
-          stack.append(ele)
-      else :
-          if d[ele] == stack[-1]:
-              stack.pop()
-          else:
-              return False 
-  if stack :
-      return False
-  else :
-      return True
