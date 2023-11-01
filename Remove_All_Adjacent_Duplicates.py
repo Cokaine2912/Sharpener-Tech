927291de0e61fa5c3bd9c5978eeac4e667d111e0
Niraj Prashant Kamble <97113663+Cokaine2912@users.noreply.github.com>
2023-11-01 18:22:47 +0530

    Create Remove_All_Adjacent_Duplicates.py

---
+++ Remove_All_Adjacent_Duplicates.py
@@ -0,0 +1,11 @@
+## REMMOVE ADJACENT DUPLICATES
+
+def removeDuplicates(self, s: str) -> str:
+    stack = []
+    for ele in s :
+        if stack and stack[-1] == ele :
+            stack.pop()
+        else :
+            stack.append(ele)
+
+    return "".join(stack)
