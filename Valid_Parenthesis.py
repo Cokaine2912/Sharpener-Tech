dd27bcc63e6e7414e0e52f44aa048123b8449c7a
Niraj Prashant Kamble <97113663+Cokaine2912@users.noreply.github.com>
2023-11-01 17:32:28 +0530

    Update Valid_Parenthesis.py

--- Valid_Parenthesis.py
+++ Valid_Parenthesis.py
@@ -1,22 +1,21 @@
 ## VALID PARENTHESIS
 
-class Solution:
-    def isValid(self, s: str) -> bool:
-        d = {")" : "(","]" : "[","}" : "{"}
-        stack = []
-        for ele in s :
-            if not stack and ele in d :
-                return False
-    
-            if ele not in d :
-                stack.append(ele)
-            else :
-                if d[ele] == stack[-1]:
-                    stack.pop()
-                else:
-                    return False
-            
-        if stack :
+def isValid(self, s: str) -> bool:
+    d = {")" : "(","]" : "[","}" : "{"}
+    stack = []
+    for ele in s :
+        if not stack and ele in d :
             return False
+
+        if ele not in d :
+            stack.append(ele)
         else :
-            return True
+            if d[ele] == stack[-1]:
+                stack.pop()
+            else:
+                return False
+        
+    if stack :
+        return False
+    else :
+        return True
