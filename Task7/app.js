b7339fe1a519d34a70c1b6ce95d27fbb53bce137
Cokaine2912 <niraj28352@gmail.com>
2024-02-10 20:33:10 +0530

    This is Task 7 update

---
+++ Task7/app.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+// Routes
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+// Controllers
+
+const errorController = require("./controllers/error");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.showError);
+
+app.listen(5000);
---
+++ Task7/models/products.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const fs = require("fs");
+
+const rootDir = require("../util/path");
+
+// const products = [];
+
+module.exports = class Product {
+  constructor(title) {
+    this.title = title;
+  }
+  save() {
+    // products.push(this);
+    const p = path.join(rootDir, "../Task6/data/products.json");
+    console.log("TO CHECK THIS !!!!!!!", p);
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
+  }
+  static fetchAll(cb) {
+    const p = path.join(rootDir, "../Task6/data/products.json");
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        cb([]);
+      }
+      cb(JSON.parse(fileContent));
+    });
+  }
+};
