e3b91560d38beac583ecf12acf69aeed83538e3a
Cokaine2912 <niraj28352@gmail.com>
2024-02-21 06:37:23 +0530

    Project Update

--- Node_Js_Test_Project/public/js/script.js
+++ Node_Js_Test_Project/public/js/script.js
@@ -20,12 +20,19 @@ function handlePosting(event) {
 }
 function handleFind(event) {
   event.preventDefault();
+  const temp = document.getElementById("avg-check-id");
+  const temp_name = document.getElementById("h4-company-name");
+  if (temp) {
+    temp.remove();
+    temp_name.remove();
+  }
   let total = 0;
 
   const display_div = document.getElementById("display");
-
+  const avg_display_div = document.createElement("div");
+  avg_display_div.id = "avg-check-id";
   const bytagname = document.querySelectorAll("li");
-  // console.log("by tag name", bytagname);
+
   bytagname.forEach((li) => {
     li.remove();
   });
@@ -33,21 +40,33 @@ function handleFind(event) {
   const obj = {
     findname: event.target.findname.value,
   };
+
+  const company_name_div = document.getElementById("display-company-name");
+
+  const company_name = document.createTextNode(`Company : ${obj.findname}`);
+
+  const h4_company = document.createElement("h4");
+  h4_company.id = "h4-company-name";
+  h4_company.appendChild(company_name);
+  company_name_div.appendChild(h4_company);
+
   axios
     .post("http://localhost:5000/find-review", obj)
     .then((op) => {
-      // console.log(op.data);
-
       for (let i = 0; i < op.data.length; i++) {
         // console.log(op.data[i]);
         total = total + op.data[i].rating;
         display(op.data[i]);
       }
       // console.log(total);
+      let avg_roundoff = total / op.data.length;
+      avg_roundoff = avg_roundoff.toPrecision(3);
       const avg_rating = document.createTextNode(
-        `Avg Rating :${total / op.data.length}`
+        `Avg Rating :${avg_roundoff}`
       );
-      display_div.appendChild(avg_rating);
+
+      avg_display_div.appendChild(avg_rating);
+      company_name_div.appendChild(avg_display_div);
     })
     .catch((err) => {
       console.log(err);
@@ -58,11 +77,21 @@ function display(obj) {
   const ul = document.getElementById("display-review");
 
   const li = document.createElement("li");
+  const hr = document.createElement("hr");
   li.id = "review-list";
   li.className = "items";
-  const li_text = document.createTextNode(
-    `Comapny Name :${obj.company} | Pros :${obj.pros} | Cons :${obj.cons} | Rating :${obj.rating}`
-  );
-  li.appendChild(li_text);
+  const stars = "⭐".repeat(obj.rating);
+
+  const prosTextNode = document.createTextNode(`Pros: ${obj.pros}`);
+  const consTextNode = document.createTextNode(`Cons: ${obj.cons}`);
+  const ratingTextNode = document.createTextNode(`Rating: ${stars}`);
+
+  li.appendChild(prosTextNode);
+  li.appendChild(document.createElement("br"));
+  li.appendChild(consTextNode);
+  li.appendChild(document.createElement("br"));
+  li.appendChild(ratingTextNode);
+
+  li.appendChild(hr);
   ul.appendChild(li);
 }
